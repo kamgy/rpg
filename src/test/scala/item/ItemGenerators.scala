6010612c768b0b48f9cc7c8eb4879f15303659b9
Kamil Głód <kamil.glod@swissborg.com>
2025-05-01 22:15:37 +0200

    Update code

--- src/test/scala/item/ItemGenerators.scala
+++ src/test/scala/item/ItemGenerators.scala
@@ -7,7 +7,6 @@ import org.scalacheck.Gen
 object ItemGenerators {
   def itemNameOpt(minLength: Int, maxLength: Int): Gen[Option[ItemName]] =
     Generators.alphaNumStrings(minLength, maxLength).map(ItemName.option.apply)
-
   def itemPriceOpt(minPrice: Int, maxPrice: Int): Gen[Option[ItemPrice]] =
     Gen.choose(minPrice, maxPrice).map(ItemPrice.option.apply)
 }
