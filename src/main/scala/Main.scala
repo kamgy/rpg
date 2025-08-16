21c90b1962d98541792571c41403fe9af66ad908
Kamil Głód <kamil.glod@swissborg.com>
2025-08-16 09:20:21 +0200

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,17 @@
 package dev.kamgy
 
+import scala.concurrent.duration.DurationInt
+
 import cats.effect.{IO, IOApp}
 
 object Main extends IOApp.Simple {
-  override val run: IO[Unit] = {
-    IO.println("Hello, world!")
-  }
+  override val run: IO[Unit] =
+    for {
+      _ <- IO.println("Creating resources...")
+      _ <- IO.sleep(500.millis)
+      _ <- IO.println("Resources created successfully!")
+      _ <- IO.println("Starting the application...")
+      _ <- IO.sleep(500.millis)
+      _ <- IO.println("Application started successfully!")
+    } yield ()
 }
